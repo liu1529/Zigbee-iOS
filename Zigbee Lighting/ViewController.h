9c4744c36ee73adf957e1e01667304b59c7cc2a2
lxl <lxl@lxldeMac.local>
2014-11-03 14:15:55 +0800

    Initial Commit

---
+++ Zigbee Lighting/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Zigbee Lighting
+//
+//  Created by lxl on 14-11-3.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Zigbee Lighting/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Zigbee Lighting
+//
+//  Created by lxl on 14-11-3.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
